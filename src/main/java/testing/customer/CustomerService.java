85f00ab8d334aebc38c42a1bc12c7521d81cf0df
Amador <amasabcar@outlook.com>
2024-06-12 15:14:13 +0200

    add logic to register new customer

--- src/main/java/testing/customer/CustomerService.java
+++ src/main/java/testing/customer/CustomerService.java
@@ -3,6 +3,8 @@ package testing.customer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class CustomerService {
@@ -10,16 +12,29 @@ public class CustomerService {
     private final CustomerRepository customerRepository;
 
     public void registerNewCustomer(CustomerRegistrationRequest request) {
-        //check if phoneNumber already exists
-        if (customerRepository.existsCustomerByPhoneNumber(request.getPhoneNumber()))
-            throw new IllegalArgumentException("Phone number already exists");
+        if (request == null) {
+            throw new IllegalArgumentException("Customer cannot be null");
+        }
 
-        Customer customer = Customer.builder()
+        //check if phoneNumber already exists
+        String phoneNumber = request.getPhoneNumber();
+
+        Optional<Customer> optionalCustomer =
+                customerRepository.findCustomerByPhoneNumberNative(phoneNumber);
+
+        if (optionalCustomer.isPresent()) {
+            //check if phone number belongs to the same customer comparing by name
+            if (optionalCustomer.get().getName().equals(request.getName()))
+                return;
+            throw new IllegalArgumentException(String.format(
+                    "Phone number [%s] is already taken", phoneNumber));
+        }
+
+        customerRepository.save(Customer.builder()
                 .name(request.getName())
                 .phoneNumber(request.getPhoneNumber())
-                .build();
-
-        customerRepository.save(customer);
+                .build()
+        );
 
     }
 
